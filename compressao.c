a9ee41220f7787fee153567e03eb685a0c73226f
Vitor Mendes <Vitor Mendes>
2017-05-08 13:40:47 -0300

    Extrair minimo e ordenar lista feitos

--- compressao.c
+++ compressao.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _TNo {
 	char ch;
 	int freq;
-	struct _TNo *prox;
-
+	struct _TNo *dir;
+	struct _TNo *esq;
 }TNo;
-
-int capturaFrequencia(char *vetor , int qtdeLetras) {
-	
-	int *freq;
-	freq = (int*) calloc(256,sizeof(int)); 
-	int converted;
-	int i = 0;
-	
-	for(i = 0; i < qtdeLetras; i++){
-		converted = (int) vetor[i];
-		freq[converted]++;
-	} 
-	return freq;	
-}
 
 TNo * adicionaInicio( TNo *inicio, char ch, int freq)
 {
@@ -28,25 +15,73 @@ TNo * adicionaInicio( TNo *inicio, char ch, int freq)
 	TNo *novo = (TNo *) calloc(1,sizeof(TNo));
 	novo->ch = ch;
 	novo->freq = freq;
-	
-	TNo *cp = (TNo *) calloc(1,sizeof(TNo));
-	cp->ch = inicio->ch;
-	cp->freq = inicio->freq;
-	cp->prox = inicio->prox;
-		
-	if(inicio == 0) {
-		novo->prox = inicio;
-		inicio = novo;
-	}
+
+	novo->dir = inicio;
+	inicio = novo;
 
 	return inicio;
 }
 
+
+TNo * adicionaOrdenado( TNo *inicio, char ch, int freq , int * ap)
+{
+	// Alocando um novo No
+	TNo *novo = (TNo *) calloc(1,sizeof(TNo));
+	novo->ch = ch;
+	novo->freq = freq;
+	
+	TNo *aux,*anterior;
+	
+	aux = inicio;
+	anterior = NULL;
+	
+	while(aux && freq > aux->freq){
+		anterior = aux;
+		aux = aux->dir;
+	}
+	novo->dir = aux;
+	if(anterior){
+		anterior->dir = novo;
+	}
+		
+	else {
+		inicio = novo;
+	}
+	
+	*ap = *ap + 1;
+			
+	return inicio;
+}
+
+
+//TNo * geraRaiz(TNo * lista , int stop){
+//	int i;
+//	
+//	for( i = 0; i < stop; i++) {
+//		TNo *novo = (TNo *) calloc(1,sizeof(TNo));
+//		novo->esq = extraiMinimo(lista);
+//		novo->dir = extraiMinimo(lista);
+//		novo->freq = novo->esq->freq + novo->dir->freq;
+//		
+//		printf("Novo no com raiz %d , direita %d , esquerda %d \n " , novo->freq , novo->dir->freq , novo->esq->freq);
+//	}
+//} 
+
+
 void mostraLista(TNo * inicio ){
 	while( inicio ){
-		printf("%d ", inicio->freq);
-		inicio = inicio->prox;
+		printf("%c=%d ", inicio->ch, inicio->freq);
+		inicio = inicio->dir;
 	}
+}
+
+TNo * extraiMinimo(TNo * inicio) {
+	
+	if(inicio->dir){
+		inicio = inicio->dir;
+	}
+	printf("Minimo  = %d \n" , inicio->freq);
+	return inicio;
 }
 
 
@@ -54,31 +89,33 @@ int main(){
 	
 	FILE *file;
 	char c; // caractere do arquivo
-	int i = 0;
-	char fc[256];
-	int *freq;
+	int i;
+	int *diffLetters = 0;
+	int fc[256];
 	TNo *ini;
 	ini = 0;
 	
 	file = fopen("base.txt" , "r");
 	
+	memset(fc, 0, 256*sizeof(int));
 	// Copia caracteres do arquivo para o vetor fc
 	if(file) {
 		while((c = getc(file)) != EOF) {
-			fc[i] = c;
-			i++;		
+			fc[c]++;
 		}
 	}
 	
-	freq = capturaFrequencia(fc , 8);
 	// Cria a linked list
 	for(i = 0; i<256; i++) {
-		if(freq[i] != 0) {
-			ini = adicionaInicio(ini , (char)i , freq[i]);
+		if(fc[i] != 0) {
+			ini = adicionaOrdenado(ini , (char)i , fc[i] , &diffLetters);
 		}
 	}
+	TNo *x = extraiMinimo(ini);
+	TNo *y = extraiMinimo(x);
+	TNo *o = extraiMinimo(y);
+	TNo *p = extraiMinimo(o);
+//	geraRaiz(ini , *diffLetters);
+//	mostraLista(ini);
 	
-	mostraLista(ini);
-
 }
-
